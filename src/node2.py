48b68ce5f663b450e29f8142e5174234b47f4513
Ioannis Dadiotis <ioannis.dadiotis@iit.it>
2021-02-19 16:19:07 +0100

    added clearance, swing velocity as rosparams. replace wait_for_msg with subscriber. update plots with publish frequency

--- src/node2.py
+++ src/node2.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import WrenchStamped
 from walking import Walking
 import numpy as np
+import math
 from centauro_contact_detection.msg import contacts as contacts_msg
 from std_msgs.msg import Bool
 
@@ -12,14 +13,19 @@ from std_msgs.msg import Bool
 R = 0.078
 
 
-def casannis(pub_freq):
+def contacts_callback (msg):
+
+    # pass to global scope
+    global sw_contact_msg
+    sw_contact_msg = msg
+
+def casannis(int_freq):
 
     """
     This function call the optimization problem constructor, the solver, the interpolator and interfaces with cartesio
     through ros topics
     Args:
-        pub_freq: desired publish frequency (affects only the interpolation and not the optimal solution)
-
+        int_freq: desired interpolation frequency (affects only the interpolation and not the optimal solution)
     Returns:
         publish the desired data to cartesio through ros topics
 
@@ -65,6 +71,15 @@ def casannis(pub_freq):
     tgt_dy = rospy.get_param("~tgt_dy")
     tgt_dz = rospy.get_param("~tgt_dz")
 
+    # Clearance to be achieved, counted from the highest point
+    swing_clear = rospy.get_param("~clear")  # get from command line as target_dx
+
+    # Swing velocity
+    swing_vel = rospy.get_param("~sw_vel")
+
+    # approximate distance covered during swing
+    tgt_ds = math.sqrt(tgt_dx**2 + tgt_dy**2 + (tgt_dy + 2*0.1)**2)
+
     # target position as array
     swing_tgt = np.array([contacts[swing_id - 1][0] + tgt_dx, contacts[swing_id - 1][1] + tgt_dy, contacts[swing_id - 1][2] + tgt_dz])
 
@@ -76,9 +91,12 @@ def casannis(pub_freq):
     # apply or no contact detection
     cont_detection = rospy.get_param("~cont_det")  # from command line as contact_det:=True/False
 
-    # Publishers for the swing foot, com in the cartesian space and contact flag
+    # Publishers for the swing foot, com in the cartesian space
     f_pub_ = rospy.Publisher('/cartesian/' + id_name[swing_id-1] + '_wheel/reference', PoseStamped, queue_size=10)
     com_pub_ = rospy.Publisher('/cartesian/com/reference', PoseStamped, queue_size=10)
+
+    # Subscriber for contact flags
+    rospy.Subscriber('/contacts', contacts_msg, contacts_callback)
 
     # Messages to be published for com and swing foot
     com_msg = PoseStamped()
@@ -94,29 +112,25 @@ def casannis(pub_freq):
     # sol is the directory returned by solve class function contains state, forces, control values
     sol = walk.solve(x0=x0, contacts=contacts, swing_id=swing_id-1, swing_tgt=swing_tgt, swing_t=swing_t, min_f=100)
 
-    # debug
-    '''print("X0 is:", x0)
-    print("contacts is:", contacts)
-    print("swing id is:", swing_id-1)
-    print("swing target is:", swing_tgt)
-    print("swing time:", swing_t)'''
-
     # interpolate the values, pass solution values and interpolation freq. (= publish freq.)
-    interpl = walk.interpolate(sol, contacts[swing_id-1], swing_tgt, swing_t, pub_freq)
+    interpl = walk.interpolate(sol, contacts[swing_id-1], swing_tgt, swing_clear, swing_t, int_freq)
 
     # All points to be published
-    N_total = int(walk._N * walk._dt * pub_freq)  # total points --> total time * frequency
-
+    N_total = int(walk._N * walk._dt * int_freq)  # total points --> total time * interpl. frequency
+    executed_trj = N_total - 1
     # contact detection
-    i = 0   # counter i for contact detection
     early_contact = False
 
     # time starting contact detection
     t_early = 0.5 * (swing_t[0] + swing_t[1])
 
+    # trj points during swing phase
+    N_swing_total = int((swing_t[1] - swing_t[0]) * int_freq)
 
+    # publish freq wrt the desired swing velocity
+    freq = swing_vel * N_swing_total / tgt_ds
 
-    rate = rospy.Rate(pub_freq)  # Frequency trj publishing
+    rate = rospy.Rate(freq)  # Frequency trj publishing
     # loop interpolation points to publish on a specified frequency
     for counter in range(N_total):
 
@@ -147,11 +161,8 @@ def casannis(pub_freq):
             # If no early contact detected
             elif not early_contact:
 
-                # receive contact flag of the swing leg
-                sw_contact_sub = rospy.wait_for_message("/contacts", contacts_msg)
-
                 # if there is contact
-                if getattr(getattr(sw_contact_sub, id_contact_name[swing_id-1]), 'data'):
+                if getattr(getattr(sw_contact_msg, id_contact_name[swing_id-1]), 'data'):
 
                     early_contact = True  # stop swing trajectory
 
@@ -197,20 +208,18 @@ def casannis(pub_freq):
             rate.sleep()'''
 
     # print the trajectories
-    if not cont_detection:
-        executed_trj = counter
     print("Early contact detected. Trj Counter is:", executed_trj, "out of total", N_total)
 
     if rospy.get_param("~plots"):
-        walk.print_trj(interpl, pub_freq, executed_trj)
+        walk.print_trj(interpl, int_freq, freq, executed_trj)
 
 
 if __name__ == '__main__':
 
-    # desired publish frequency
-    freq = 500
+    # desired interpolation frequency
+    interpolation_freq = 300
 
     try:
-        casannis(freq)
+        casannis(interpolation_freq)
     except rospy.ROSInterruptException:
         pass
