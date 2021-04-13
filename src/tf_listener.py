be0f1c0e7326ac18ab97130da48dcc81bbadb965
Ioannis Dadiotis <ioannis.dadiotis@iit.it>
2021-04-13 20:14:34 +0200

    tf2 listener

--- src/tf_listener.py
+++ src/tf_listener.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 
-import tf
+import tf2_ros
 from geometry_msgs.msg import TransformStamped
 
 
@@ -12,31 +12,33 @@ def get_transform(frame, base_frame):
     transform_pub = rospy.Publisher(publ_topic, TransformStamped, queue_size=10)
 
     # construct listener
-    #tfBuffer = tf2_ros.Buffer()
-    #listener = tf2_ros.TransformListener(tfBuffer)
-    listener = tf.TransformListener()
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+    #listener = tf.TransformListener()
 
     # define a msg
-    trans_msg = TransformStamped()
+    #trans_msg = TransformStamped()
 
     # receive in a loop
     rate = rospy.Rate(200.0)
     while not rospy.is_shutdown():
         try:
-            #trans = tfBuffer.lookup_transform(frame, base_frame, rospy.Time())
-            (trans, rot) = listener.lookupTransform(frame, base_frame, rospy.Time(0))
+            trans = tfBuffer.lookup_transform(frame, base_frame, rospy.Time())
+            #(trans, rot) = listener.lookupTransform(frame, base_frame, rospy.Time(0))
 
-            trans_msg.transform.translation.x = trans[0]
+            '''trans_msg.transform.translation.x = trans[0]
             trans_msg.transform.translation.y = trans[1]
             trans_msg.transform.translation.z = trans[2]
             trans_msg.transform.rotation.x = rot[0]
             trans_msg.transform.rotation.y = rot[1]
             trans_msg.transform.rotation.z = rot[2]
-            trans_msg.transform.rotation.w = rot[3]
+            trans_msg.transform.rotation.w = rot[3]'''
 
-            transform_pub.publish(trans_msg)
+            #transform_pub.publish(trans_msg)
 
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            transform_pub.publish(trans)
+
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             rate.sleep()
             continue
 
