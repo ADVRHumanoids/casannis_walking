94c4a51c342f7b3fa98ad53c3216a76aadaa9152
Arturo Laurenzi <ioannis.dadiotis@iit.it>
2021-10-29 16:07:17 +0200

    scripts for rosbag forces and stepup 0.3 m platform

---
+++ bash/rosbag_forces.sh
@@ -0,0 +1,1 @@
+rosbag record /cartesian/force_estimation/contact_1 /cartesian/force_estimation/contact_2 /cartesian/force_estimation/contact_3 /cartesian/force_estimation/contact_4
---
+++ bash/stepup_steps.bash
@@ -0,0 +1,5 @@
+mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.3, 0.3, 0.3, 0.3]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.23, 0.23, 0.23, 0.23]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.22, 0.22, 0.22, 0.22]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.3, 0.3, 0.3, 0.3]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[3,4,1,2]" clearance:=0.1
+
