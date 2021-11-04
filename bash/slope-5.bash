b41af1e5bc0c2cc3edfcdf328a9b83acc6c03f72
Ioannis Dadiotis <ioannis.dadiotis@iit.it>
2021-11-04 12:44:39 +0100

    added bash files for slope experiments

---
+++ bash/slope-5.bash
@@ -0,0 +1,4 @@
+rosparam load $(rospack find yiannis_centauro_pytools)/yaml/slope_homing_rod_plates.yaml cartesian/home
+mon launch yiannis_centauro_pytools initialize_cartesio.launch imu_available:=false
+mon launch casannis_walking gait_payload.launch slope_x:=-5 target_dx:="[0.2, 0.2, 0.2, 0.2]" min_force:=175
+
