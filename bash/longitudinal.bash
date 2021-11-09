8eadc64bb7b6f9ceab7dbebea9d714b144fae410
Ioannis Dadiotis <ioannis.dadiotis@iit.it>
2021-11-09 18:55:34 +0100

    added bash files for longitudinal, lateral experiments. trial_stepup has slow last 2-step optimization (to be tried)

---
+++ bash/longitudinal.bash
@@ -0,0 +1,2 @@
+mon launch casannis_walking gait_payload.launch min_force:=185 target_dx:="[0.3,0.3,0.3,0.3]" clearance:=0.1 swing_id:="[4,2,3,1]" plots:=true
+
--- bash/stepup_steps.bash
+++ bash/stepup_steps.bash
@@ -1,5 +1,5 @@
 mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.3, 0.3, 0.3, 0.3]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
 mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.25, 0.25, 0.23, 0.23]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
 mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.25, 0.25, 0.22, 0.22]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
-mon launch casannis_walking gait_payload.launch min_force:=250 target_dx:="[0.25, 0.25, 0.25, 0.25]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[3,4]" clearance:=0.1 plots:=true publish_until:=4.0
+mon launch casannis_walking gait_payload.launch min_force:=225 target_dx:="[0.25, 0.25, 0.25, 0.25]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[3,4]" clearance:=0.1 plots:=true
 
