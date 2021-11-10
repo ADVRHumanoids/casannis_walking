8962834e7e2b3698a956fb327e4a665c669f8f54
Arturo Laurenzi <ioannis.dadiotis@iit.it>
2021-11-10 17:49:18 +0100

    this didn't work with 2 sec of final stance phase, although separate steps worked

--- bash/trial_step_up.bash
+++ bash/trial_step_up.bash
@@ -1,5 +1,4 @@
 mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.3, 0.3, 0.3, 0.3]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
 mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.25, 0.25, 0.23, 0.23]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
 mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.25, 0.25, 0.22, 0.22]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
-mon launch casannis_walking gait_payload.launch min_force:=220 target_dx:="[0.25, 0.25, 0.25, 0.25]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[3, 4]" clearance:=0.1 plots:=true swing_t1:="[3.0,6.0]" swing_t2:="[8.0,13.0]"
-
+mon launch casannis_walking gait_payload.launch min_force:=220 target_dx:="[0.25, 0.25, 0.25, 0.25]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[3, 4]" clearance:=0.1 plots:=true swing_t1:="[1.0,3.0]" swing_t2:="[5.0,10.0]"
