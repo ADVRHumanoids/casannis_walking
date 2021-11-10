61bdcd191708d43d4a7b3f6a4484c96a56aed840
Arturo Laurenzi <ioannis.dadiotis@iit.it>
2021-11-10 18:45:40 +0100

    increase lateral (feasible on robot)

--- bash/lateral.bash
+++ bash/lateral.bash
@@ -1,2 +1,2 @@
-mon launch casannis_walking gait_payload.launch min_force:=185 target_dx:="[0.0,0.0,0.0,0.0]" target_dy:="[0.2,0.2,0.2,0.2]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=185 target_dx:="[0.0,0.0,0.0,0.0]" target_dy:="[0.25,0.25,0.25,0.25]" clearance:=0.1
 
