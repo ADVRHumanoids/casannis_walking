5cd971c6aed84dd20ee813b951cb5ac4a1ccb026
Ioannis Dadiotis <ioannis.dadiotis@iit.it>
2021-11-15 12:43:23 +0100

    add file to reproduce simulation stepup

---
+++ bash/simul_stepup.bash
@@ -0,0 +1,6 @@
+mon launch casannis_walking gait_payload.launch min_force:=100 target_dx:="[0.3, 0.3, 0.3, 0.3]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=100 target_dx:="[0.25, 0.25, 0.23, 0.23]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=100 target_dx:="[0.25, 0.25, 0.22, 0.22]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=100 target_dx:="[0.25, 0.25, 0.3, 0.3]" target_dz:="[0.3,0.3]" swing_id:="[3,4]" clearance:=0.1
+
+
