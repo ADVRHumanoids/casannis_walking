44d820170210321563bd41a515b4de9f1f82acb2
Arturo Laurenzi <ioannis.dadiotis@iit.it>
2021-11-17 14:47:32 +0100

    updated friction of saved urdf 2x10 with ballhands, added bashfiles for 3 locomotion only and other stepups

---
+++ bash/slow_stepup.bash
@@ -0,0 +1,5 @@
+mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.3, 0.3, 0.3, 0.3]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1 box_far_from_chest:=False
+mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.25, 0.25, 0.23, 0.23]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.25, 0.25, 0.22, 0.22]" target_dz:="[0.0,0.0,0.0,0.0]" swing_id:="[1,2,3,4]" clearance:=0.1
+mon launch casannis_walking gait_payload.launch min_force:=175 target_dx:="[0.25, 0.25, 0.25, 0.25]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[3]" clearance:=0.1 
+mon launch casannis_walking gait_payload.launch min_force:=220 target_dx:="[0.25, 0.25, 0.25, 0.25]" target_dz:="[0.3,0.3,0.0,0.0]" swing_id:="[4]" clearance:=0.1 plots:=true swing_t1:="[3.0,8.0]"
