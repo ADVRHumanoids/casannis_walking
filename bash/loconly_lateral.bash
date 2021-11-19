ae28e17d50872679f701e928e4ee624fc0b7ad7c
Ioannis Dadiotis <ioannis.dadiotis@iit.it>
2021-11-19 14:53:32 +0100

    paradoxically locomotion only optimization is infeasible with 185 N threshold. reduced to 175

--- bash/loconly_lateral.bash
+++ bash/loconly_lateral.bash
@@ -1,1 +1,1 @@
-mon launch casannis_walking gait.launch min_force:=185 target_dx:="[0.0,0.0,0.0,0.0]" target_dy:="[0.2,0.2,0.2,0.2]" clearance:=0.1 robot_mass:=129
+mon launch casannis_walking gait.launch min_force:=175 target_dx:="[0.0,0.0,0.0,0.0]" target_dy:="[0.2,0.2,0.2,0.2]" clearance:=0.1 robot_mass:=129
--- bash/loconly_longitudinal.bash
+++ bash/loconly_longitudinal.bash
@@ -1,1 +1,1 @@
-mon launch casannis_walking gait.launch min_force:=185 target_dx:="[0.3,0.3,0.3,0.3]" clearance:=0.1 swing_id:="[4,2,3,1]" robot_mass:=129 plots:=true
+mon launch casannis_walking gait.launch min_force:=175 target_dx:="[0.3,0.3,0.3,0.3]" clearance:=0.1 swing_id:="[4,2,3,1]" robot_mass:=129 plots:=true
