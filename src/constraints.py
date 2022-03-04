fixed swing_tgt to be list of np.arrays and not list of lists, this created wrong calculation of mean of contact positions

src/constraints.py
@@ -504,9 +504,18 @@ def set_contact_parameters(contacts, swing_id, swing_target, clearance_times,
 
 
 def get_nominal_CoM_bounds_from_contacts(contacts, offset_from_payload=0):
+    '''
+    Get a nominal com region from the contact positions
+    Args:
+        contacts: the current contacts, should be a list of numpy.array (not a list of lists)
+        offset_from_payload: vertical offset, to be passed if we want to compensate for payload
+
+    Returns:
+        List of 2 np.arrays of 9 elements (pos, vel, acc), always zero velocity and acceleration,
+        thus it is currently only suitable for final knot constraints
+    '''
 
     sum_contacts = sum([sublist for sublist in contacts])
-
     mean_hor_foothold = [0.25 * coordinate for coordinate in sum_contacts]
 
     final_position_l = [mean_hor_foothold[0]] + [mean_hor_foothold[1]] + \

src/gait_node.py
@@ -113,9 +113,10 @@ def casannis(int_freq):
     for i in range(step_num):
 
         # targets
-        swing_tgt.append([contacts[swing_id[i] - 1][0] + tgt_dx[i],
-                          contacts[swing_id[i] - 1][1] + tgt_dy[i],
-                          contacts[swing_id[i] - 1][2] + tgt_dz[i]])
+        swing_tgt.append(np.array([contacts[swing_id[i] - 1][0] + tgt_dx[i],
+                                   contacts[swing_id[i] - 1][1] + tgt_dy[i],
+                                   contacts[swing_id[i] - 1][2] + tgt_dz[i]
+                                   ]))
         # swing phases
         swing_t.append(rospy.get_param("~sw_t" + str(i+1)))  # from command line as swing_t:="[a,b]"
         swing_t[i] = swing_t[i].rstrip(']').lstrip('[').split(',')  # convert swing_t from "[a, b]" to [a,b]