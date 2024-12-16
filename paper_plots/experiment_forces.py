84f42e9ccba82fe67f5dcbdb61574068eba4bc1f
IoannisDadiotis <ioannis.dadiotis@iit.it>
2024-12-16 15:51:07 +0100

    recover plots made with python2

--- paper_plots/experiment_forces.py
+++ paper_plots/experiment_forces.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python2
+
 import pickle
-import matplotlib.pyplot as plt
-import matplotlib
+import matplotlib.pyplot as plt 
+import matplotlib 
 import numpy as np
+import matplotlib.gridspec as gridspec
 
 
 if __name__ == "__main__":
 
     # get data from stepup experiment saved in yiannis_centauro_pytools module
-    with open('/home/ioannis/centauro_ws/src/yiannis_centauro_pytools/txt/stepup_forces.txt', 'rb') as f:
+    with open('/home/idadiotis/ocs2_ws/src/yiannis_centauro_pytools/txt/stepup_forces.txt', 'rb') as f:
         estimated_forces = pickle.load(f)
     # momentum_based estimated forces
-    with open('/home/ioannis/centauro_ws/src/yiannis_centauro_pytools/txt/stepup_momentumbased_forces.txt', 'rb') as f:
+    with open('/home/idadiotis/ocs2_ws/src/yiannis_centauro_pytools/txt/stepup_momentumbased_forces.txt', 'rb') as f:
         momentumbased_estimated_forces = pickle.load(f)
 
     swing_timings = {'1': [], '2': [], '3': [], '4': []}
@@ -18,7 +21,7 @@ if __name__ == "__main__":
     force_sol = []
     planned_time_trj = []
     for i, name in enumerate(['1', '2', '3', '4', '5']):
-        with open('../paper_plots/stepup' + name + '.txt', 'rb') as f:
+        with open('/home/idadiotis/centauro_ws/src/casannis_walking/paper_plots/stepup' + name + '.txt', 'rb') as f:
             solution = pickle.load(f)
             force_sol += solution['F']
         if i == 0 or i == 1 or i == 2:
@@ -69,24 +72,41 @@ if __name__ == "__main__":
         plt.legend([str(name) + '_x', str(name) + '_y', str(name) + '_z'])
     plt.xlabel('Time [s]')
 
+    fontsize_axes =  40
+    fontsize_legends = 0.6 * 32
+    linewidths = 4
+    ticksize =28
     # planned vs estimated force
     for j, compon in enumerate(coords):
-        plt.figure()
-        matplotlib.rc('xtick', labelsize=16)
-        matplotlib.rc('ytick', labelsize=16)
+        # smaller figure
+        fig = plt.figure(figsize=(15, 10))
+        # Create a GridSpec object with 3 rows and 2 columns
+        gs = gridspec.GridSpec(4, 1)  # Adjust height_ratios as needed
+        # Create the subplots
+        ax1 = plt.subplot(gs[0, 0])  
+        ax2 = plt.subplot(gs[1, 0])  
+        ax3 = plt.subplot(gs[2, 0])  
+        ax4 = plt.subplot(gs[3, 0])
+        ax = [ax1, ax2, ax3, ax4]
         for i in range(leg_number):     # loop over legs
-            plt.subplot(4, 1, i+1)
-            plt.plot(time_trj, [filt_value[j] for filt_value in estimated_forces[i]][:int(end_estimation*1000)],      # filtered
-                     color='r', label='est', linewidth=2)
-            plt.plot(time_trj, [filt_value[j] for filt_value in momentumbased_estimated_forces[i]][:int(end_estimation*1000)],      # momentum based
-                     ':', color='g', label='est', linewidth=1)
-            plt.plot(planned_time_trj, force_sol[3 * i + j::12], '-', color='b', label='plan', linewidth=3)
-            plt.xlabel('Time $[s]$', fontsize=20)
-            plt.xlim([0.0, end_estimation+2.0])
-            plt.ylabel(leg_names[i] + ' force $[N]$', fontsize=20)
-            plt.grid()
+            ax[i].plot(time_trj, [filt_value[j] for filt_value in estimated_forces[i]][:int(end_estimation*1000)],      # filtered
+                     color='r', label='estimated', linewidth=linewidths)
+            # plt.plot(time_trj, [filt_value[j] for filt_value in momentumbased_estimated_forces[i]][:int(end_estimation*1000)],      # momentum based
+            #          ':', color='g', label='est', linewidth=1)
+            ax[i].plot(planned_time_trj, force_sol[3 * i + j::12], '-', color='b', label='planned', linewidth=linewidths)
+            ax[i].set_xlim([0.0, end_estimation+2.0])
+            ax[i].set_ylabel(leg_names[i] + ' $[N]$', fontsize=fontsize_axes)
+            ax[i].grid(True, axis='both', which='major', linestyle='--', alpha=0.7)
+            ax[i].tick_params(axis='both', labelsize=ticksize)
             for ii in range(len(swing_timings[str(i+1)])):
-                plt.axvspan(swing_timings[str(i+1)][ii][0], swing_timings[str(i+1)][ii][1], alpha=0.2)
-        plt.legend(fontsize=15)
-        plt.suptitle('F' + compon + ' estimation', fontsize=20)
+                ax[i].axvspan(swing_timings[str(i+1)][ii][0], swing_timings[str(i+1)][ii][1], alpha=0.5, color='grey')
+            if i < leg_number - 1:
+                # ax[i].set_xticks([])  # Remove x-ticks
+                # ax[i].xaxis.grid(True)
+                ax[i].grid(True)
+                ax[i].set_xticklabels([])
+            else:
+                ax[i].set_xlabel('Time $[s]$', fontsize=fontsize_axes)
+        plt.legend(fontsize=fontsize_legends, loc="upper right")
+        plt.suptitle('F' + compon + ' estimation', fontsize=fontsize_axes)
     plt.show()
